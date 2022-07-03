98c4f021db6b5e45902746eab6ce25f06ae160b0
jaslaptop <jaskarannagra@hotmail.com>
2022-07-03 09:07:24 -0700

    fix formatting

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,19 +1,16 @@
-
 const body = document.querySelector("body");
-const bigBox = document.createElement('div');
+const bigBox = document.createElement("div");
 body.appendChild(bigBox);
 
-bigBox.classList.add("bigbox")
-for (let i=0; i < (16**2); i++){
-const gridBox = document.createElement('div');
-bigBox.appendChild(gridBox);
-gridBox.classList.add("box", "item");
-gridBox.addEventListener("mouseover",(e)=>{
-    if(e.buttons == 1){
-        console.log(e.buttons)
-    gridBox.classList.add("hover")}
-})
-};
-
-
-
+bigBox.classList.add("bigbox");
+for (let i = 0; i < 16 ** 2; i++) {
+  const gridBox = document.createElement("div");
+  bigBox.appendChild(gridBox);
+  gridBox.classList.add("box", "item");
+  gridBox.addEventListener("mouseover", (e) => {
+    if (e.buttons == 1) {
+      console.log(e.buttons);
+      gridBox.classList.add("hover");
+    }
+  });
+}
