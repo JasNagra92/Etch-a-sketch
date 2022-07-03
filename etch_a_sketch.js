a5a4c2996406b4ff663f7a282e49616f9e7a093b
jaslaptop <jaskarannagra@hotmail.com>
2022-07-03 16:54:19 -0700

    create new container to house grid, can now make title at top of page

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,36 +1,35 @@
-const body = document.querySelector("body");
+const container = document.querySelector("#flex-container");
 const bigBox = document.createElement("div");
-body.appendChild(bigBox);
+container.appendChild(bigBox);
 
 bigBox.classList.add("bigbox");
-function restart(num){
+function restart(num) {
   let boxArray = document.querySelectorAll(".item");
-  boxArray.forEach(box=>{
+  boxArray.forEach((box) => {
     box.remove();
+  });
+  for (let i = 0; i < num ** 2; i++) {
+    const gridBox = document.createElement("div");
+    bigBox.appendChild(gridBox);
+    gridBox.classList.add("box", "item");
+    gridBox.style.width = 512 / num + "px";
+    gridBox.style.height = 512 / num + "px";
+    gridBox.addEventListener("mousedown", (e) => {
+      if (e.buttons == 1) {
+        console.log(e.buttons);
+        gridBox.classList.add("hover");
+      }
+    });
+    gridBox.addEventListener("mouseover", (e) => {
+      if (e.buttons == 1) {
+        console.log(e.buttons);
+        gridBox.classList.add("hover");
+      }
+    });
   }
-  );
-for (let i = 0; i < num ** 2; i++) {
-  const gridBox = document.createElement("div");
-  bigBox.appendChild(gridBox);
-  gridBox.classList.add("box", "item");
-  gridBox.style.width = (512/num)+"px";
-  gridBox.style.height = (512/num)+"px";
-  gridBox.addEventListener("mousedown", (e) => {
-    if (e.buttons == 1) {
-      console.log(e.buttons);
-      gridBox.classList.add("hover");
-    }
-  });
-  gridBox.addEventListener("mouseover", (e) => {
-    if (e.buttons == 1) {
-      console.log(e.buttons);
-      gridBox.classList.add("hover");
-    }
-  });
 }
-}
-let btn = document.querySelector("button")
-btn.addEventListener('click',(e)=>{
+let btn = document.querySelector("button");
+btn.addEventListener("click", (e) => {
   let size = prompt("how many squares accros for sketchpad?");
   restart(size);
-})
+});
