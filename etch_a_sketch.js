75fcc79bd3272c7c7e5582d20f7f45999b5cd3af
jaslaptop <jaskarannagra@hotmail.com>
2022-07-03 09:12:19 -0700

    add mousedown event to fix bug

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -7,6 +7,12 @@ for (let i = 0; i < 16 ** 2; i++) {
   const gridBox = document.createElement("div");
   bigBox.appendChild(gridBox);
   gridBox.classList.add("box", "item");
+  gridBox.addEventListener("mousedown", (e) => {
+    if (e.buttons == 1) {
+      console.log(e.buttons);
+      gridBox.classList.add("hover");
+    }
+  });
   gridBox.addEventListener("mouseover", (e) => {
     if (e.buttons == 1) {
       console.log(e.buttons);
