e252b259ffc7d4ba8764b2473316344361eb8bd4
jaslaptop <jaskarannagra@hotmail.com>
2022-07-03 14:27:15 -0700

    Add choose grid size button so user can adjust size of grid with a prompt. Resets grid after each press.

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -3,10 +3,18 @@ const bigBox = document.createElement("div");
 body.appendChild(bigBox);
 
 bigBox.classList.add("bigbox");
-for (let i = 0; i < 16 ** 2; i++) {
+function restart(num){
+  let boxArray = document.querySelectorAll(".item");
+  boxArray.forEach(box=>{
+    box.remove();
+  }
+  );
+for (let i = 0; i < num ** 2; i++) {
   const gridBox = document.createElement("div");
   bigBox.appendChild(gridBox);
   gridBox.classList.add("box", "item");
+  gridBox.style.width = (512/num)+"px";
+  gridBox.style.height = (512/num)+"px";
   gridBox.addEventListener("mousedown", (e) => {
     if (e.buttons == 1) {
       console.log(e.buttons);
@@ -20,3 +28,9 @@ for (let i = 0; i < 16 ** 2; i++) {
     }
   });
 }
+}
+let btn = document.querySelector("button")
+btn.addEventListener('click',(e)=>{
+  let size = prompt("how many squares accros for sketchpad?");
+  restart(size);
+})
