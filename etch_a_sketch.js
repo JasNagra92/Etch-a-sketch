3d81412f0fe676311530ffbfaa93c2f08c40b4a0
jaslaptop <jaskarannagra@hotmail.com>
2022-07-03 09:05:35 -0700

    add if statement to check if mouse button is held down, will only add hover class if it returns true

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -8,8 +8,10 @@ for (let i=0; i < (16**2); i++){
 const gridBox = document.createElement('div');
 bigBox.appendChild(gridBox);
 gridBox.classList.add("box", "item");
-gridBox.addEventListener("mouseover",()=>{
-    gridBox.classList.add("hover")
+gridBox.addEventListener("mouseover",(e)=>{
+    if(e.buttons == 1){
+        console.log(e.buttons)
+    gridBox.classList.add("hover")}
 })
 };
 
